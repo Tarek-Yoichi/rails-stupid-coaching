abd9996b7f4dd8ecf71ea309891da73f3ae134e7
Tarek Maraoui <jumanga.contact@gmail.com>
2022-08-15 12:39:25 +0200

    exo2

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -6,8 +6,4 @@ class QuestionsTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Questions"
   # end
-  test "visiting /ask renders the form" do
-    visit ask_url
-    assert_selector "p", text: "Ask your coach anything"
-  end
 end
